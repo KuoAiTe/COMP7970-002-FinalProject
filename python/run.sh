ea80bd38f6fdae586893d22383d3185ce139b22a
KuoAiTe <robinsa87@gmail.com>
2017-11-18 23:06:42 -0600

    ShellScript

--- python/run.sh
+++ python/run.sh
@@ -4,13 +4,14 @@ inputFileName="BlogCatalog.mat"
 outputFileName="SDE-$inputFileName"
 dataFile="$datafolder$inputFileName"
 outputFile="$datafolder$outputFileName"
+repeat=2
 k=500
 portion=0.9
 function run(){
-  python3 kmeans.py -f "$dataFile" -k "$k" -o "$outputFile"
+  #python3 kmeans.py -f "$dataFile" -k "$k" -o "$outputFile"
   python3 linearSVM.py -g "$dataFile" -s "$outputFile" -p "$portion"
 }
-run
-#for i in {1..100}; do
-#  run
-#done
+for (( i=0; i<$repeat; i++ ))
+do
+  run
+done
