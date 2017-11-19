16a56bc697a591e46d16f5e1a869578a365cf5cb
KuoAiTe <robinsa87@gmail.com>
2017-11-18 22:48:43 -0600

    shellscript

--- python/run.sh
+++ python/run.sh
@@ -8,7 +8,7 @@ k=500
 portion=0.9
 function run(){
   python3 kmeans.py -f "$dataFile" -k "$k" -o "$outputFile"
-  #python3 linearSVM.py -g "$dataFile" -s "$outputFile" -p "$portion"
+  python3 linearSVM.py -g "$dataFile" -s "$outputFile" -p "$portion"
 }
 run
 #for i in {1..100}; do
