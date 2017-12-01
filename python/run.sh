c413c8b72077a7b8d30d71145d252bb84b290916
KuoAiTe <robinsa87@gmail.com>
2017-11-30 19:40:29 -0600

    fix bug

--- python/run.sh
+++ python/run.sh
@@ -8,7 +8,7 @@ repeat=1
 k=5000
 portion=0.9
 function run(){
-  #python3 kmeans.py -f "$dataFile" -k "$k" -o "$outputFile"
+  python3 kmeans.py -f "$dataFile" -k "$k" -o "$outputFile"
   python3 linearSVM.py -g "$dataFile" -s "$outputFile" -p "$portion"
 }
 for (( i=0; i<$repeat; i++ ))
